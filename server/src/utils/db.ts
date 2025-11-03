6fa7beeb13f7e18e105ee0c85788f3fd9f114ef7
gautamkumar1 <gautamraaz936@gmail.com>
2025-11-03 23:57:12 +0530

    created all imp controller for sessions

---
+++ server/src/utils/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '../generated/prisma/client.js';
+
+const prisma = new PrismaClient({
+});
+
+async function connectDB() {
+  try {
+    await prisma.$connect();
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('❌ Error connecting to the database:', error);
+  }
+}
+
+export { prisma, connectDB };
