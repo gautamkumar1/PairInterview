57cbea4f31f3183db3676f4944c4849e753f2893
gautamkumar1 <gautamraaz936@gmail.com>
2025-11-05 19:29:25 +0530

    added google auth

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -11,6 +11,12 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
     hooks: {
         after: createAuthMiddleware(async (ctx) => {
             if(ctx.path.startsWith("/sign-up") && ctx.context.newSession){
